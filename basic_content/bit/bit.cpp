6770e8a313984017d66e84ae1e09021d6a719ba7
Eric Zeng <szeng74@gatech.edu>
2025-05-22 00:24:12 -0400

    assert, bit

--- basic_content/bit/bit.cpp
+++ basic_content/bit/bit.cpp
@@ -8,7 +8,17 @@ struct stuff {
   unsigned int : 0;
   unsigned int field3 : 3;
 };
+
+struct stuff2 {
+  unsigned int field1 : 30;
+  unsigned int : 2;
+  unsigned int field2 : 4;
+  unsigned int field3 : 3;
+};
+
 int main() {
+  constexpr int s1 = sizeof(stuff);
+  constexpr int s2 = sizeof(stuff2);
   struct stuff s = {1, 3, 5};
   cout << s.field1 << endl;
   cout << s.field2 << endl;
